67f9ad2b8ab79f87f41829ac5cf8e4a3e44bcaad
ewdlop <ray810815@gmail.com>
2020-05-19 01:48:27 -0400

    Neural Netowrk basic classes in C#

---
+++ NeuralNetowkInCSharp/Models/NNDataSet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetowkInCSharp.Models
+{
+    public class NNDataSet
+    {
+        public double[] Values { get; set; }
+        public double[] Targets { get; set; }
+        public NNDataSet(double[] values, double[] targets)
+        {
+            Values = values;
+            Targets = targets;
+        }
+    }
+}
