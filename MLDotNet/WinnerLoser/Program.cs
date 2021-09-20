5b450382124a4cb11c887a6cb652b81e7597ff60
ewdlop <ray810815@gmail.com>
2021-09-20 00:44:39 -0400

    WinnerLoser of Infer.Net from MS documentation

---
+++ MLDotNet/WinnerLoser/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace WinnerLoser
+{
+    using System;
+    using System.Linq;
+    using Microsoft.ML.Probabilistic;
+    using Microsoft.ML.Probabilistic.Distributions;
+    using Microsoft.ML.Probabilistic.Models;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // The winner and loser in each of 6 samples games
+            var winnerData = new[] { 0, 0, 0, 1, 3, 4 };
+            var loserData = new[] { 1, 3, 4, 2, 1, 2 };
+
+            // Define the statistical model as a probabilistic program
+            var game = new Microsoft.ML.Probabilistic.Models.Range(winnerData.Length);
+            var player = new Microsoft.ML.Probabilistic.Models.Range(winnerData.Concat(loserData).Max() + 1);
+            var playerSkills = Variable.Array<double>(player);
+            playerSkills[player] = Variable.GaussianFromMeanAndVariance(6, 9).ForEach(player);
+
+            var winners = Variable.Array<int>(game);
+            var losers = Variable.Array<int>(game);
+
+            using (Variable.ForEach(game))
+            {
+                // The player performance is a noisy version of their skill
+                var winnerPerformance = Variable.GaussianFromMeanAndVariance(playerSkills[winners[game]], 1.0);
+                var loserPerformance = Variable.GaussianFromMeanAndVariance(playerSkills[losers[game]], 1.0);
+
+                // The winner performed better in this game
+                Variable.ConstrainTrue(winnerPerformance > loserPerformance);
+            }
+
+            // Attach the data to the model
+            winners.ObservedValue = winnerData;
+            losers.ObservedValue = loserData;
+
+            // Run inference
+            var inferenceEngine = new InferenceEngine();
+            var inferredSkills = inferenceEngine.Infer<Gaussian[]>(playerSkills);
+
+            // The inferred skills are uncertain, which is captured in their variance
+            var orderedPlayerSkills = inferredSkills
+                .Select((s, i) => new { Player = i, Skill = s })
+                .OrderByDescending(ps => ps.Skill.GetMean());
+
+            foreach (var playerSkill in orderedPlayerSkills)
+            {
+                Console.WriteLine($"Player {playerSkill.Player} skill: {playerSkill.Skill}");
+            }
+
+        }
+    }
+}
