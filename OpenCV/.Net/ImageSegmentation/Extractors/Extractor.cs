7da7ace2d7d5c5a714de092f685351bc56d71223
ewdlop <ray810815@gmail.com>
2022-07-31 14:26:56 -0400

    trying out cv2

---
+++ OpenCV/.Net/ImageSegmentation/Extractors/Extractor.cs
@@ -0,0 +1,37 @@
+﻿using OpenCvSharp;
+
+namespace ImageSegmentation.Extractors;
+
+/// <summary>
+/// https://debuggercafe.com/image-foreground-extraction-using-opencv-contour-detection/
+/// </summary>
+public static class Extractor
+{
+    public static Mat<Point>? FindLargetsContour(Mat image)
+    {
+        Mat<Point>[] contours = Cv2.FindContoursAsMat(
+            image, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+
+        return contours.MaxBy(c => c.ContourArea());
+    }
+
+    public static Point[] FindLargetsContourToArray(Mat image)
+    {
+        Mat<Point>[] contours = Cv2.FindContoursAsMat(
+            image, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+
+        return contours.MaxBy(c => c.ContourArea()).ToArray();
+    }
+
+    public static void ApplyNewBackground(string backImagePath, string outputImagePath, InputArray mask3d, InputArray foreground)
+    {
+        //type conversion issue...
+        //List<double> x = new List<double>();
+        //OutputArray outputArray = OutputArray.Create(x);
+        //Cv2.Multiply(mask3d, foreground, outputArray);
+        //Mat background = Cv2.ImRead(backImagePath);
+        //Cv2.Resize(background,background, new Size(foreground.Rows(), foreground.Cols()) );
+        //Cv2.Add(foreground, background, outputArray);
+        //Cv2.ImWrite(outputImagePath, Mat.FromArray<double> (outputArray));
+    }
+}
--- OpenCV/.Net/ImageSegmentation/Program.cs
+++ OpenCV/.Net/ImageSegmentation/Program.cs
@@ -1,34 +1,86 @@
-﻿using OpenCvSharp;
+﻿using Bogus;
+using ImageSegmentation.Extractors;
+using OpenCvSharp;
 using SkiaSharp;
+using System.Collections.Immutable;
+//DectectFacesAsync();
+CropImage();
 
-string assetsPath = Path.Combine(Environment.CurrentDirectory, "Assets");
-string facexmlPath = Path.Combine(assetsPath, "haarcascade_frontalface_alt2.xml");
-string imagesPath = Path.Combine(assetsPath, "Images");
-string imageName = "family.jpg";
-string imageOutputName = "family_new.png";
-
-using CascadeClassifier haarCascade = new CascadeClassifier(facexmlPath);
-
-// Load target image
-using Mat gray = new Mat(Path.Combine(imagesPath, imageName), ImreadModes.Grayscale);
-
-// Detect faces
-Rect[] faces = haarCascade.DetectMultiScale(
-                    gray, 1.08, 2, HaarDetectionTypes.ScaleImage, new Size(30, 30));
-
-byte[] bytes = await File.ReadAllBytesAsync(Path.Combine(imagesPath, imageName));
-SKBitmap bitmap = SKBitmap.Decode(bytes);
-using SKCanvas canvas = new SKCanvas(bitmap);
-SKPaint sKPaint = new SKPaint()
+void CropImage()
 {
-    Color = SKColors.Red,
-    Style = SKPaintStyle.Stroke
-};
-for (int i = 0; i < faces.Length; i++)
-{
-    Rect rect = faces[i];
-    canvas.DrawRect(rect.X, rect.Y, rect.Width, rect.Height, sKPaint);
+    string assetsPath = Path.Combine(Environment.CurrentDirectory, "Assets");
+    string facexmlPath = Path.Combine(assetsPath, "haarcascade_frontalface_alt2.xml");
+    string imagesPath = Path.Combine(assetsPath, "Images");
+    string imagePath = Path.Combine(imagesPath, "Sparrow-PNG-Image-71269-768x622.png");
+    string imageOutputName = "fauci_test.png";
+
+    using Mat image = Cv2.ImRead(imagePath);
+    using Mat image2 = Cv2.ImRead(imagePath);
+    using Mat blurred = new Mat();
+    using Mat gray = new Mat();
+    using Mat threshold = new Mat();
+    
+    for (int i = 0; i < 256; i++)
+    {
+        try
+        {
+            //Cv2.GaussianBlur(image, blurred, new Size(i, i), 0);
+            Cv2.CvtColor(image, gray, ColorConversionCodes.BGR2GRAY);
+            Cv2.Threshold(gray, threshold, i, 255, ThresholdTypes.Otsu);
+            Point[] contour = Extractor.FindLargetsContourToArray(threshold);
+            //Point[][] contours = Cv2.FindContoursAsArray(
+            //    threshold, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+            Cv2.DrawContours(
+                image2,
+                contours: new Point[1][] { contour },
+                -1,
+                new Scalar(0, 0, 255),
+                2,
+                LineTypes.AntiAlias,
+                maxLevel: 1);
+            Cv2.ImWrite($"Test/{i}.jpg", image2);
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+
+    //Cv2.ImShow(imageOutputName, image2);
+    //Cv2.WaitKey(0);
+
 }
-canvas.Flush();
-using SKData data = bitmap.Encode(SKEncodedImageFormat.Png, 100);
-await File.WriteAllBytesAsync(imageOutputName, data.ToArray());
+
+async Task DectectFacesAsync()
+{
+    string assetsPath = Path.Combine(Environment.CurrentDirectory, "Assets");
+    string facexmlPath = Path.Combine(assetsPath, "haarcascade_frontalface_alt2.xml");
+    string imagesPath = Path.Combine(assetsPath, "Images");
+    string imageName = "family.jpg";
+    string imageOutputName = "family_new.png";
+    using CascadeClassifier haarCascade = new CascadeClassifier(facexmlPath);
+
+    // Load target image
+    using Mat gray = new Mat(Path.Combine(imagesPath, imageName), ImreadModes.Grayscale);
+
+    // Detect faces
+    Rect[] faces = haarCascade.DetectMultiScale(
+                        gray, 1.08, 2, HaarDetectionTypes.ScaleImage, new Size(30, 30));
+
+    byte[] bytes = await File.ReadAllBytesAsync(Path.Combine(imagesPath, imageName));
+    SKBitmap bitmap = SKBitmap.Decode(bytes);
+    using SKCanvas canvas = new SKCanvas(bitmap);
+    SKPaint sKPaint = new SKPaint()
+    {
+        Color = SKColors.Red,
+        Style = SKPaintStyle.Stroke
+    };
+    for (int i = 0; i < faces.Length; i++)
+    {
+        Rect rect = faces[i];
+        canvas.DrawRect(rect.X, rect.Y, rect.Width, rect.Height, sKPaint);
+    }
+    canvas.Flush();
+    using SKData data = bitmap.Encode(SKEncodedImageFormat.Png, 100);
+    await File.WriteAllBytesAsync(imageOutputName, data.ToArray());
+}
