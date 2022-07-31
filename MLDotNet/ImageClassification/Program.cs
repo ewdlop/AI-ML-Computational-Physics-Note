9886b47aa5d5659d40a9fc270db4f95affa6a1c6
ewdlop <ray810815@gmail.com>
2022-07-30 22:45:12 -0400

    dll error?

---
+++ MLDotNet/ImageClassification/Program.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+
+
+string assetsPath = Path.Combine(Environment.CurrentDirectory, "Assets");
+string imagesFolder = Path.Combine(assetsPath, "Images");
+string trainTagsTsv = Path.Combine(imagesFolder, "tags.tsv");
+string testTagsTsv = Path.Combine(imagesFolder, "test-tags.tsv");
+string predictSingleImage = Path.Combine(imagesFolder, "toaster3.jpg");
+string inceptionTensorFlowModel = Path.Combine(assetsPath, "Inception", "tensorflow_inception_graph.pb");
+
+MLContext mlContext = new MLContext();
+IDataView testData = mlContext.Data.LoadFromTextFile<ImageData>(path: testTagsTsv, hasHeader: false);
+ITransformer model = GenerateModel(mlContext);
+ClassifySingleImage(mlContext, model);
+
+ITransformer GenerateModel(MLContext mlContext)
+{
+    // L-BFGS
+    //https://en.wikipedia.org/wiki/Limited-memory_BFGS
+    //Broyden–Fletcher–Goldfarb–Shanno algorithm
+    //https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm
+    IEstimator<ITransformer> pipeline = mlContext.Transforms.LoadImages(outputColumnName: "input", imageFolder: imagesFolder, inputColumnName: nameof(ImageData.ImagePath))
+                // The image transforms transform the images into the model's expected format.
+                .Append(mlContext.Transforms.ResizeImages(
+                    outputColumnName: "input",
+                    imageWidth: InceptionSettings.ImageWidth,
+                    imageHeight: InceptionSettings.ImageHeight,
+                    inputColumnName: "input"))
+                .Append(mlContext.Transforms.ExtractPixels(
+                    outputColumnName: "input",
+                    interleavePixelColors: InceptionSettings.ChannelsLast,
+                    offsetImage: InceptionSettings.Mean))
+                .Append(mlContext.Model.LoadTensorFlowModel(inceptionTensorFlowModel).
+                    ScoreTensorFlowModel(
+                    outputColumnNames: new[] { "softmax2_pre_activation" },
+                    inputColumnNames: new[] { "input" },
+                    addBatchDimensionInput: true))
+                .Append(mlContext.Transforms.Conversion.MapValueToKey(
+                    outputColumnName: "LabelKey", 
+                    inputColumnName: "Label"))
+                .Append(mlContext.MulticlassClassification.Trainers.LbfgsMaximumEntropy(
+                    labelColumnName: "LabelKey",
+                    featureColumnName: "softmax2_pre_activation"))
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue(
+                    outputColumnName: "PredictedLabelValue", 
+                    inputColumnName: "PredictedLabel"))
+                .AppendCacheCheckpoint(mlContext);
+
+    IDataView trainingData = mlContext.Data.LoadFromTextFile<ImageData>(path: trainTagsTsv, hasHeader: false);
+    //The Fit() method trains your model by applying the training dataset to the pipeline
+    ITransformer model = pipeline.Fit(trainingData);
+    IDataView predictions = model.Transform(testData);
+    // Create an IEnumerable for the predictions for displaying results
+    IEnumerable<ImagePrediction> imagePredictionData = mlContext.Data.CreateEnumerable<ImagePrediction>(predictions, true);
+    DisplayResults(imagePredictionData);
+    MulticlassClassificationMetrics metrics =
+        mlContext.MulticlassClassification.Evaluate(predictions,
+            labelColumnName: "LabelKey",
+            predictedLabelColumnName: "PredictedLabel");
+    Console.WriteLine($"LogLoss is: {metrics.LogLoss}");
+    Console.WriteLine($"PerClassLogLoss is: {string.Join(" , ", metrics.PerClassLogLoss.Select(c => c.ToString()))}");
+    return model;
+}
+void ClassifySingleImage(MLContext mlContext, ITransformer model)
+{
+    ImageData imageData = new()
+    {
+        ImagePath = predictSingleImage
+    };
+
+    // Make prediction function (input = ImageData, output = ImagePrediction)
+    using PredictionEngine<ImageData, ImagePrediction> predictor = mlContext.Model.CreatePredictionEngine<ImageData, ImagePrediction>(model);
+    ImagePrediction prediction = predictor.Predict(imageData);
+    Console.WriteLine($"Image: {Path.GetFileName(imageData.ImagePath)} predicted as: {prediction.PredictedLabelValue} with score: {prediction.Score.Max()} ");
+}
+void DisplayResults(IEnumerable<ImagePrediction> imagePredictionData)
+{
+    foreach (ImagePrediction prediction in imagePredictionData)
+    {
+        Console.WriteLine($"Image: {Path.GetFileName(prediction.ImagePath)} predicted as: {prediction.PredictedLabelValue} with score: {prediction.Score.Max()} ");
+    }
+}
+struct InceptionSettings
+{
+    public const int ImageHeight = 224;
+    public const int ImageWidth = 224;
+    public const float Mean = 117;
+    public const float Scale = 1;
+    public const bool ChannelsLast = true;
+}
+public class ImagePrediction : ImageData
+{
+    public float[] Score;
+
+    public string PredictedLabelValue;
+}
+public class ImageData
+{
+    [LoadColumn(0)]
+    public string ImagePath;
+
+    [LoadColumn(1)]
+    public string Label;
+}
---
+++ MLDotNet/ImageClassificationTransferLearning/Program.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.ML;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Vision;
+
+string workspaceRelativePath = Path.Combine(Environment.CurrentDirectory, "workspace");
+string assetsRelativePath = Path.Combine(Environment.CurrentDirectory, "Assets");
+MLContext mlContext = new MLContext();
+IEnumerable<ImageData> images = LoadImagesFromDirectory(
+    folder: assetsRelativePath, 
+    useFolderNameAsLabel: true);
+IDataView imageData = mlContext.Data.LoadFromEnumerable(images);
+IDataView shuffledData = mlContext.Data.ShuffleRows(imageData);
+Microsoft.ML.Data.EstimatorChain<Microsoft.ML.Data.ImageLoadingTransformer>
+    preprocessingPipeline = mlContext.Transforms.Conversion.MapValueToKey(
+        inputColumnName: "Label",
+        outputColumnName: "LabelAsKey")
+    .Append(mlContext.Transforms.LoadRawImageBytes(
+        outputColumnName: "Image",
+        imageFolder: assetsRelativePath,
+        inputColumnName: "ImagePath"));
+IDataView preProcessedData = preprocessingPipeline
+                    .Fit(shuffledData)
+                    .Transform(shuffledData);
+TrainTestData trainSplit = mlContext.Data.TrainTestSplit(
+    data: preProcessedData, 
+    testFraction: 0.3);
+TrainTestData validationTestSplit = mlContext.Data.TrainTestSplit(
+    data: trainSplit.TestSet);
+
+IDataView trainSet = trainSplit.TrainSet;
+IDataView validationSet = validationTestSplit.TrainSet;
+IDataView testSet = validationTestSplit.TestSet;
+
+//https://en.wikipedia.org/wiki/Residual_neural_network
+//
+ImageClassificationTrainer.Options classifierOptions = new ImageClassificationTrainer.Options()
+{
+    FeatureColumnName = "Image",
+    LabelColumnName = "LabelAsKey",
+    ValidationSet = validationSet,
+    Arch = ImageClassificationTrainer.Architecture.ResnetV2101,
+    MetricsCallback = (metrics) => Console.WriteLine(metrics),
+    TestOnTrainSet = false,
+    ReuseTrainSetBottleneckCachedValues = true,
+    ReuseValidationSetBottleneckCachedValues = true
+};
+Microsoft.ML.Data.EstimatorChain<Microsoft.ML.Transforms.KeyToValueMappingTransformer>? trainingPipeline = 
+    mlContext.MulticlassClassification.Trainers.ImageClassification(classifierOptions)
+        .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+ITransformer trainedModel = trainingPipeline.Fit(trainSet);
+ClassifySingleImage(mlContext, testSet, trainedModel);
+ClassifyImages(mlContext, testSet, trainedModel);
+
+void ClassifySingleImage(MLContext mlContext, IDataView data, ITransformer trainedModel)
+{
+    using PredictionEngine<ModelInput, ModelOutput> predictionEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(trainedModel);
+    ModelInput image = mlContext.Data.CreateEnumerable<ModelInput>(data, reuseRowObject: true).First();
+    ModelOutput prediction = predictionEngine.Predict(image);
+    Console.WriteLine("Classifying single image");
+    OutputPrediction(prediction);
+}
+
+void ClassifyImages(MLContext mlContext, IDataView data, ITransformer trainedModel)
+{
+    IDataView predictionData = trainedModel.Transform(data);
+    IEnumerable<ModelOutput> predictions = mlContext.Data.CreateEnumerable<ModelOutput>(
+        data: predictionData, 
+        reuseRowObject: true).Take(10);
+    Console.WriteLine("Classifying multiple images");
+    foreach (ModelOutput prediction in predictions)
+    {
+        OutputPrediction(prediction);
+    }
+}
+
+
+void OutputPrediction(ModelOutput prediction)
+{
+    string imageName = Path.GetFileName(prediction.ImagePath);
+    Console.WriteLine($"Image: {imageName} | Actual Value: {prediction.Label} | Predicted Value: {prediction.PredictedLabel}");
+}
+
+IEnumerable<ImageData> LoadImagesFromDirectory(string folder, bool useFolderNameAsLabel = true)
+{
+    string[] files = Directory.GetFiles(folder, "*",
+        searchOption: SearchOption.AllDirectories);
+    for (int i = 0; i < files.Length; i++)
+    {
+        string file = files[i];
+        if ((Path.GetExtension(file) != ".jpg") && (Path.GetExtension(file) != ".png"))
+        { 
+            continue;
+        }
+        string label = Path.GetFileName(file);
+        DirectoryInfo? parentDirectory = Directory.GetParent(file);
+        if (parentDirectory is not null)
+        {
+            if (useFolderNameAsLabel)
+            {
+                label = parentDirectory.Name;
+            }
+            else
+            {
+                for (int index = 0; index < label.Length; index++)
+                {
+                    if (!char.IsLetter(label[index]))
+                    {
+                        label = label[..index];
+                        break;
+                    }
+                }
+            }
+        }
+        yield return new ImageData()
+        {
+            ImagePath = file,
+            Label = label
+        };
+    }
+}
+
+class ImageData
+{
+    public string ImagePath { get; set; }
+
+    public string Label { get; set; }
+}
+
+class ModelInput
+{
+    public byte[] Image { get; set; }
+
+    public uint LabelAsKey { get; set; }
+
+    public string ImagePath { get; set; }
+
+    public string Label { get; set; }
+}
+
+class ModelOutput
+{
+    public string ImagePath { get; set; }
+
+    public string Label { get; set; }
+
+    public string PredictedLabel { get; set; }
+}
