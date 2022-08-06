2f783c89a36c7e8964b8d9c0d89afc2719192c04
ewdlop <ray810815@gmail.com>
2022-08-06 09:01:59 -0400

    cannot use torchsharp

---
+++ CNTK/LSTM/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Test
+{
+    //Inverted Index
+    public static Dictionary<string, List<int>> InvertedIndex(string[] words)
+    {
+        Dictionary<string, List<int>> result = new Dictionary<string, List<int>>();
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (result.ContainsKey(words[i]))
+            {
+                result[words[i]].Add(i);
+            }
+            else
+            {
+                result.Add(words[i], new List<int>(1) { i });
+            }
+        }
+        return result;
+    }
+}
---
+++ Pytorch/TorchSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using TorchSharp;
+using static torch.nn;
+
+Test();
+
+void Test()
+{
+    //var lin1 = Linear(1000, 100);
+    //var lin2 = Linear(100, 10);
+
+    //var seq = Sequential(("lin1", lin1), ("relu1", ReLU()), ("drop1", Dropout(0.1)), ("lin2", lin2));
+    //var x = torch.randn(64, 1000);
+    //var y = torch.randn(64, 10);
+
+    ////optimize gradient descent
+    //var optimizer = torch.optim.Adam(seq.parameters());
+    ////Rectified Adam optimizer
+    //var optimizer = torch.optim.RAdam(seq.parameters());
+    //var loss = functional.mse_loss(Reduction.Sum);
+}
