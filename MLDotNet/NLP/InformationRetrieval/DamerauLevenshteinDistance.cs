86795d0cbbab0c2ba5f8df106bbca4ddc59d11b8
ewdlop <ray810815@gmail.com>
2022-08-13 22:57:28 -0400

    DamerauLevenshteinDistance

---
+++ MLDotNet/NLP/InformationRetrieval/DamerauLevenshteinDistance.cs
@@ -0,0 +1,241 @@
+﻿namespace InformationRetrieval;
+
+public static class DamerauLevenshteinDistance
+{
+    /// <summary>
+    /// Optimal Damerau–Levenshtein distance.
+    /// <para><see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance"></see></para>
+    /// <para>See also: <see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance#Optimal_string_alignment_distance"></see></para>
+    /// <para>Default is case sensitive</para>
+    /// <para>Levenshtein distance is a measure of the difference between two sequences.</para>
+    /// <para>The number of edit operations needed to make the strings equal under the condition that no substring is edited more than once.</para>
+    /// <para>Triangle inequality does not hold.</para>
+    /// <para>One dimensional Array is used to store the distance matrix.</para>
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static int OptimalDamerauLevenshteinDistance(this string s, string t)
+    {
+        int n = s.Length;
+        int m = t.Length;
+        int[] d = new int[(n + 1) * (m + 1)];
+
+        // Step 1
+        if (n == 0) return m;
+        if (m == 0) return n;
+
+        // Step 2
+        for (int i = 0; i <= n; d[i * m + 1] = i++) { }
+        for (int j = 0; j <= m; d[j] = j++) { }
+
+        // Step 3
+        for (int i = 1; i <= n; i++)
+        {
+            //Step 4
+            for (int j = 1; j <= m; j++)
+            {
+                // Step 5
+                int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                // Step 6
+                d[i * (m + 1) + j] = Math.Min(
+                    Math.Min(d[(i - 1) * (m + 1) + j] + 1, // deletion
+                             d[i * (m + 1) + j - 1] + 1), // insertion
+                             d[(i - 1) * (m + 1) + j - 1] + cost);  // substitution
+
+                // Step 7
+                if (i > 1 && j > 1 && t[j - 1] == s[i - 2] && s[i - 1] == t[j - 2])
+                {
+                    d[i * (m + 1) + j] = Math.Min(d[i * (m + 1) + j],
+                                                  d[(i - 2) * (m + 1) + j - 2] + cost); // transposition
+                }
+            }
+        }
+
+        // Step 8
+        return d[n * (m + 1) + m];
+    }
+    /// <summary>
+    /// Optimal Damerau–Levenshtein distance.
+    /// <para><see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance"></see></para>
+    /// <para>See also: <see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance#Optimal_string_alignment_distance"></see></para>
+    /// <para>Default is case sensitive</para>
+    /// <para>Levenshtein distance is a measure of the difference between two sequences.</para>
+    /// <para>The number of edit operations needed to make the strings equal under the condition that no substring is edited more than once.</para>
+    /// <para>Triangle inequality does not hold.</para>
+    /// <para>One dimensional Array is used to store the distance matrix.</para>
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static int OptimalDamerauLevenshteinDistance(this ReadOnlySpan<char> s, ReadOnlySpan<char> t)
+    {
+        int n = s.Length;
+        int m = t.Length;
+        int[] d = new int[(n + 1) * (m + 1)];
+
+        // Step 1
+        if (n == 0) return m;
+        if (m == 0) return n;
+
+        // Step 2
+        for (int i = 0; i <= n; d[i * m + 1] = i++) { }
+        for (int j = 0; j <= m; d[j] = j++) { }
+
+        // Step 3
+        for (int i = 1; i <= n; i++)
+        {
+            //Step 4
+            for (int j = 1; j <= m; j++)
+            {
+                // Step 5
+                int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                // Step 6
+                d[i * (m + 1) + j] = Math.Min(
+                    Math.Min(d[(i - 1) * (m + 1) + j] + 1, // deletion
+                             d[i * (m + 1) + j - 1] + 1), // insertion
+                             d[(i - 1) * (m + 1) + j - 1] + cost);  // substitution
+
+                // Step 7
+                if (i > 1 && j > 1 && t[j - 1] == s[i - 2] && s[i - 1] == t[j - 2])
+                {
+                    d[i * (m + 1) + j] = Math.Min(d[i * (m + 1) + j],
+                                                  d[(i - 2) * (m + 1) + j - 2] + cost); // transposition
+                }
+            }
+        }
+
+        // Step 8
+        return d[n * (m + 1) + m];
+    }
+    /// <summary>
+    /// Optimal Damerau–Levenshtein distance.
+    /// <para><see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance"></see></para>
+    /// <para>See also: <see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance#Optimal_string_alignment_distance"></see></para>
+    /// <para>Default is case sensitive</para>
+    /// <para>Levenshtein distance is a measure of the difference between two sequences.</para>
+    /// <para>The number of edit operations needed to make the strings equal under the condition that no substring is edited more than once.</para>
+    /// <para>Triangle inequality does not hold.</para>
+    /// <para>One dimensional Array is used to store the distance matrix.</para>
+    /// <para>Use stack allocation</para>
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static int OptimalDamerauLevenshteinDistanceStackAlloc(this string s, string t)
+    {
+        int n = s.Length;
+        int m = t.Length;
+        Span<int> d = stackalloc int[(n + 1) * (m + 1)];
+
+        // Step 1
+        if (n == 0) return m;
+        if (m == 0) return n;
+
+        // Step 2
+        for (int i = 0; i <= n; d[i * m + 1] = i++) { }
+        for (int j = 0; j <= m; d[j] = j++) { }
+
+        // Step 3
+        for (int i = 1; i <= n; i++)
+        {
+            //Step 4
+            for (int j = 1; j <= m; j++)
+            {
+                // Step 5
+                int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                // Step 6
+                d[i * (m + 1) + j] = Math.Min(
+                    Math.Min(d[(i - 1) * (m + 1) + j] + 1, // deletion
+                             d[i * (m + 1) + j - 1] + 1), // insertion
+                             d[(i - 1) * (m + 1) + j - 1] + cost);  // substitution
+
+                // Step 7
+                if (i > 1 && j > 1 && t[j - 1] == s[i - 2] && s[i - 1] == t[j - 2])
+                {
+                    d[i * (m + 1) + j] = Math.Min(d[i * (m + 1) + j],
+                                                  d[(i - 2) * (m + 1) + j - 2] + cost); // transposition
+                }
+            }
+        }
+
+        // Step 8
+        return d[n * (m + 1) + m];
+    }
+    /// <summary>
+    /// Optimal Damerau–Levenshtein distance.
+    /// <para><see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance"></see></para>
+    /// <para>See also: <see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance#Optimal_string_alignment_distance"></see></para>
+    /// <para>Default is case sensitive</para>
+    /// <para>Levenshtein distance is a measure of the difference between two sequences.</para>
+    /// <para>The number of edit operations needed to make the strings equal under the condition that no substring is edited more than once.</para>
+    /// <para>Triangle inequality does not hold.</para>
+    /// <para>One dimensional Array is used to store the distance matrix.</para>
+    /// <para>Use stack allocation</para>
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    /// <summary>
+    /// Optimal Damerau–Levenshtein distance.
+    /// <para><see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance"></see></para>
+    /// <para>See also: <see href="https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance#Optimal_string_alignment_distance"></see></para>
+    /// <para>Default is case sensitive</para>
+    /// <para>Levenshtein distance is a measure of the difference between two sequences.</para>
+    /// <para>The number of edit operations needed to make the strings equal under the condition that no substring is edited more than once.</para>
+    /// <para>Triangle inequality does not hold.</para>
+    /// <para>One dimensional Array is used to store the distance matrix.</para>
+    /// <para>Use stack allocation</para>
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public static int OptimalDamerauLevenshteinDistanceStackAlloc(this ReadOnlySpan<char> s, ReadOnlySpan<char> t)
+    {
+        int n = s.Length;
+        int m = t.Length;
+        Span<int> d = stackalloc int[(n + 1) * (m + 1)];
+
+        // Step 1
+        if (n == 0) return m;
+        if (m == 0) return n;
+
+        // Step 2
+        for (int i = 0; i <= n; d[i * m + 1] = i++) { }
+        for (int j = 0; j <= m; d[j] = j++) { }
+
+        // Step 3
+        for (int i = 1; i <= n; i++)
+        {
+            //Step 4
+            for (int j = 1; j <= m; j++)
+            {
+                // Step 5
+                int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                // Step 6
+                d[i * (m + 1) + j] = Math.Min(
+                    Math.Min(d[(i - 1) * (m + 1) + j] + 1, // deletion
+                             d[i * (m + 1) + j - 1] + 1), // insertion
+                             d[(i - 1) * (m + 1) + j - 1] + cost);  // substitution
+
+                // Step 7
+                if (i > 1 && j > 1 && t[j - 1] == s[i - 2] && s[i - 1] == t[j - 2])
+                {
+                    d[i * (m + 1) + j] = Math.Min(d[i * (m + 1) + j],
+                                                  d[(i - 2) * (m + 1) + j - 2] + cost); // transposition
+                }
+            }
+        }
+
+        // Step 8
+        return d[n * (m + 1) + m];
+    }
+    public static bool OptimalDamerauLevenshteinDistanceThreshold(this string s, string t, int threshold)
+    {
+        return OptimalDamerauLevenshteinDistance(s, t) <= threshold;
+    }
+}
--- MLDotNet/NLP/InformationRetrieval/Indexer.cs
+++ MLDotNet/NLP/InformationRetrieval/Indexer.cs
@@ -1,16 +1,46 @@
-﻿namespace InformationRetrieval;
+﻿using System.Collections.Generic;
 
-public class Indexer
+namespace InformationRetrieval;
+
+public static class Indexer
 {
     private static readonly char[] Separators = new char[] { ' ', '\t', '\n' };
     //private readonly Dictionary<string, (int documentFrequnecy, Dictionary<int, List<int>> termDocumentPositions)> positionalIndex = new();
+
+
+    /// <summary>
+    /// Inverted Index
+    /// </summary>
+    /// <param name="documents"></param>
+    public static Dictionary<string, (int documentFrequnecy, HashSet<int> documentIDs)> InvertedIndex(this string[] documents)
+    {
+        Dictionary<string, (int documentFrequnecy, HashSet<int> documentIDs)> invertedIndex = new();
+        for (int i = 0; i < documents.Length; i++)
+        {
+            string[] words = documents[i].Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int j = 0; j < words.Length; j++)
+            {
+                string word = words[j];
+                if (invertedIndex.TryGetValue(word, out (int documentFrequnecy, HashSet<int> documentIDs) existing))
+                {
+                    invertedIndex[word] = (existing.documentFrequnecy + 1,
+                        existing.documentIDs.Append(i).OrderBy(i => i).ToHashSet());
+                }
+                else
+                {
+                    invertedIndex[word] = (1, new HashSet<int>(i));
+                }
+            }
+        }
+        return invertedIndex;
+    }
 
     /// <summary>
     /// Positional Index
     /// https://www.youtube.com/watch?v=QVVvx_Csd2I&list=PLaZQkZp6WhWwoDuD6pQCmgVyDbUWl_ZUi&index=6
     /// </summary>
     /// <param name="documents"></param>
-    public static void PositionalIndex(string[] documents)
+    public static Dictionary<string, (int documentFrequnecy, Dictionary<int, List<int>> documentTermPositions)> PositionalIndex(this string[] documents)
     {
         Dictionary<string, (int documentFrequnecy, Dictionary<int, List<int>> documentTermPositions)> positionalIndex = new();
         for (int i = 0; i < documents.Length; i++)
@@ -39,5 +69,14 @@ public class Indexer
                 }
             }
         }
+        return positionalIndex;
     }
+
+    public static IEnumerable<HashSet<int>> PhraseQuery(this Dictionary<string, (int documentFrequnecy, HashSet<int> documentIDs)> invertedIndex, string search, int threshold)
+    {
+        return invertedIndex.Where(s => s.Key.OptimalDamerauLevenshteinDistanceThreshold(search, threshold))
+            .Select(s => s.Value.documentIDs);
+    }
+
+    
 }
