8f7d1e6349d978fd8da1e925bc9f1ed4c2d77074
ewdlop <ray810815@gmail.com>
2023-09-23 11:36:53 -0400

    Azure SemanticAI notes

--- SemanticKernel/Program.cs
+++ SemanticKernel/Program.cs
@@ -1,57 +1,69 @@
 ﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Orchestration;
 using Microsoft.SemanticKernel.SkillDefinition;
-
-KernelBuilder builder = new KernelBuilder();
-builder.WithOpenAIChatCompletionService(
-         "gpt-3.5-turbo",// OpenAI Model name
-         "...your OpenAI API Key...");// OpenAI API Key
-
-IKernel kernel = builder.Build();C
+using Microsoft.SemanticKernel.Skills.Core;
 
 {
+    KernelBuilder builder = new KernelBuilder();
+    builder.WithOpenAIChatCompletionService(
+             "gpt-3.5-turbo",// OpenAI Model name
+             "...your OpenAI API Key...");// OpenAI API Key
 
-string prompt = @"{{$input}}
+    IKernel kernel = builder.Build();
+
+    {
+
+
+        string prompt = @"{{$input}}
 
 One line TLDR with the fewest words.";
 
-ISKFunction summarize = kernel.CreateSemanticFunction(prompt, maxTokens: 100);
+        ISKFunction summarize = kernel.CreateSemanticFunction(prompt, maxTokens: 100);
 
-string text1 = @"
+        string text1 = @"
 1st Law of Thermodynamics - Energy cannot be created or destroyed.
 2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
 3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
 
-string text2 = @"
+        string text2 = @"
 1. An object at rest remains at rest, and an object in motion remains in motion at constant speed and in a straight line unless acted on by an unbalanced force.
 2. The acceleration of an object depends on the mass of the object and the amount of force applied.
 3. Whenever one object exerts a force on another object, the second object exerts an equal and opposite on the first.";
 
-Console.WriteLine(await summarize.InvokeAsync(text1));
-Console.WriteLine(await summarize.InvokeAsync(text2));
-}
+        Console.WriteLine(await summarize.InvokeAsync(text1));
+        Console.WriteLine(await summarize.InvokeAsync(text2));
+    }
 
-{
-    string translationPrompt = @"{{$input}}
+    {
+        string translationPrompt = @"{{$input}}
 
 Translate the text to math.";
 
-    string summarizePrompt = @"{{$input}}
+        string summarizePrompt = @"{{$input}}
 
 Give me a TLDR with the fewest words.";
 
-    ISKFunction translator = kernel.CreateSemanticFunction(translationPrompt, maxTokens: 200);
-    ISKFunction summarize = kernel.CreateSemanticFunction(summarizePrompt, maxTokens: 100);
+        ISKFunction translator = kernel.CreateSemanticFunction(translationPrompt, maxTokens: 200);
+        ISKFunction summarize = kernel.CreateSemanticFunction(summarizePrompt, maxTokens: 100);
 
-    string inputText = @"
+        string inputText = @"
 1st Law of Thermodynamics - Energy cannot be created or destroyed.
 2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
 3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
 
-    // Run two prompts in sequence (prompt chaining)
-    SKContext output = await kernel.RunAsync(inputText, translator, summarize);
+        // Run two prompts in sequence (prompt chaining)
+        SKContext output = await kernel.RunAsync(inputText, translator, summarize);
 
-    Console.WriteLine(output);
+        Console.WriteLine(output);
 
-    // Output: ΔE = 0, ΔSuniv > 0, S = 0 at 0K.
+        // Output: ΔE = 0, ΔSuniv > 0, S = 0 at 0K.
+    }
+}
+{
+    IKernel kernel = Kernel.Builder
+        .Build();
+    IDictionary<string, ISKFunction> time = kernel.ImportSkill(new TimeSkill());
+    SKContext result = await kernel.RunAsync(time["Today"]);
+
+    Console.WriteLine(result);
 }
