73de3618cc594981ede647b877f18f3636490403
ewdlop <ray810815@gmail.com>
2023-12-21 08:37:49 -0500

    TimeEmbedding torchsarp module

---
+++ TorchSharpSD/Program.cs
@@ -0,0 +1,23 @@
+﻿using TorchSharp;
+using static TorchSharp.torch.nn;
+
+var lin1 = Linear(1000, 100);
+var lin2 = Linear(100, 10);
+var seq = Sequential(("lin1", lin1), ("relu1", ReLU()), ("drop1", Dropout(0.1)), ("lin2", lin2));
+
+var x = torch.randn(64, 1000);
+var y = torch.randn(64, 10);
+
+var optimizer = torch.optim.Adam(seq.parameters());
+
+for (int i = 0; i < 10; i++)
+{
+    var eval = seq.forward(x);
+    var output = functional.mse_loss(eval, y, Reduction.Sum);
+
+    optimizer.zero_grad();
+
+    output.backward();
+
+    optimizer.step();
+}
---
+++ TorchSharpSD/TimeEmbedding.cs
@@ -0,0 +1,28 @@
+﻿using TorchSharp.Modules;
+using static TorchSharp.torch;
+using static TorchSharp.torch.nn;
+
+//https://github.com/hkproj/pytorch-stable-diffusion/blob/main/sd/diffusion.py
+public class TimeEmbedding(string Name, int NEmbedding) : Module(Name)
+{
+    public Linear Linear1 => Linear(NEmbedding, 4 * NEmbedding);
+    public Linear Linear2 => Linear(4 * NEmbedding, 4 * NEmbedding);
+
+    public Tensor Forward(Tensor x)
+    {
+        // x: (1, 320)
+        
+        // (1, 320) -> (1, 1280)
+        
+        var result = Linear1.forward(x);
+        
+        // (1, 1280) -> (1, 1280)
+        
+        result = functional.silu(result); ;
+
+        // (1, 1280) -> (1, 1280)
+        result = Linear2.forward(x);
+
+        return result;
+    }
+}
